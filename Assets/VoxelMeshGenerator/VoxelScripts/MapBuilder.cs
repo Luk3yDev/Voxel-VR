cbba16972e6293739950b2c3ded9dab73a517f74
Luk3yDev <lukemadsen501@gmail.com>
2025-02-11 14:53:33 +1300

    Voxel stuff basics

---
+++ Assets/VoxelMeshGenerator/VoxelScripts/MapBuilder.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapBuilder : MonoBehaviour
+{
+    [SerializeField] int chunkSize;
+    [SerializeField] Vector3Int mapSize;
+    [SerializeField] VoxelMeshBuilder chunkPrefab;
+    [SerializeField] Voxel airVoxel;
+
+    NoiseGenerator noiseGenerator;
+    Voxel[,,] voxelData;
+    Dictionary<Vector3Int, VoxelMeshBuilder> builtChunks;
+    int realChunkSize;
+    private void Awake()
+    {
+        noiseGenerator = GetComponent<NoiseGenerator>();
+    }
+
+    private void Start()
+    {
+        GenerateMap();
+        BuildMap();
+    }
+
+    public void SetVoxel(Vector3Int voxelPos, Voxel voxel)
+    {
+        if (voxelPos.x >= 1 && voxelPos.x < voxelData.GetLength(0) - 1)
+            if (voxelPos.y >= 1 && voxelPos.y < voxelData.GetLength(1) - 1)
+                if (voxelPos.z >= 1 && voxelPos.z < voxelData.GetLength(2) - 1)
+                {
+                    voxelData[voxelPos.x, voxelPos.y, voxelPos.z] = voxel;
+                    Vector3Int chunkPos = GetChunkPosOfVoxel(voxelPos);
+                    Vector3Int[] neighbourChunks = new Vector3Int[]
+                    {
+                        new Vector3Int(0, 0, 0),
+                        new Vector3Int(1, 0, 0),
+                        new Vector3Int(0, 1, 0),
+                        new Vector3Int(0, 0, 1),
+                        new Vector3Int(-1, 0, 0),
+                        new Vector3Int(0, -1, 0),
+                        new Vector3Int(0, 0, -1),
+                    };
+                    foreach (Vector3Int offset in neighbourChunks )
+                    {
+                        if (builtChunks.TryGetValue((chunkPos + offset) * realChunkSize, out VoxelMeshBuilder chunk))
+                        {
+                            chunk.BuildChunk(getChunkDataFromChunkPos((chunkPos + offset) * realChunkSize));
+                        }
+                    }
+                }
+    }
+
+    public Voxel GetVoxel(Vector3Int voxelPos)
+    {
+        if (voxelPos.x >= 1 && voxelPos.x < voxelData.GetLength(0) - 1)
+            if (voxelPos.y >= 1 && voxelPos.y < voxelData.GetLength(1) - 1)
+                if (voxelPos.z >= 1 && voxelPos.z < voxelData.GetLength(2) - 1)
+                {
+                    return voxelData[voxelPos.x, voxelPos.y, voxelPos.z];
+                }
+        return null;
+    }
+
+    void GenerateMap()
+    {
+        realChunkSize = chunkSize - 2;
+        voxelData = new Voxel[mapSize.x * realChunkSize + 2, mapSize.y * realChunkSize + 2, mapSize.z * realChunkSize + 2];
+        for (int x = 0; x < mapSize.x * realChunkSize + 2; x++)
+        {
+            for (int y = 0; y < mapSize.y * realChunkSize + 2; y++)
+            {
+                for (int z = 0; z < mapSize.z * realChunkSize + 2; z++)
+                {
+                    voxelData[x, y, z] = noiseGenerator.GetVoxelAtPos(new Vector3Int(x, y, z));
+                    if (x == 0 || x == mapSize.x * realChunkSize + 1) voxelData[x, y, z] = airVoxel;
+                    if (y == 0 || y == mapSize.y * realChunkSize + 1) voxelData[x, y, z] = airVoxel;
+                    if (z == 0 || z == mapSize.z * realChunkSize + 1) voxelData[x, y, z] = airVoxel;
+                }
+            }
+        }
+
+    }
+
+    void BuildMap()
+    {
+        builtChunks = new Dictionary<Vector3Int, VoxelMeshBuilder>();
+        for (int chunkX = 0; chunkX < mapSize.x; chunkX++)
+        {
+            for (int chunkY = 0; chunkY < mapSize.y; chunkY++)
+            {
+                for (int chunkZ = 0; chunkZ < mapSize.z; chunkZ++)
+                {
+                    BuildChunk(chunkX, chunkY, chunkZ);
+                }
+            }
+        }
+    }
+
+    Voxel[,,] getChunkDataFromChunkPos(Vector3Int chunkPos)
+    {
+        Voxel[,,] chunkData = new Voxel[chunkSize, chunkSize, chunkSize];
+        for (int x = 0; x < chunkSize; x++)
+        {
+            for (int y = 0; y < chunkSize; y++)
+            {
+                for (int z = 0; z < chunkSize; z++)
+                {
+                    Vector3Int voxelWorldPos = new Vector3Int(chunkPos.x + x, chunkPos.y + y, chunkPos.z + z);
+                    chunkData[x, y, z] = voxelData[voxelWorldPos.x, voxelWorldPos.y, voxelWorldPos.z];
+                }
+            }
+        }
+        return chunkData;
+    }
+
+    void BuildChunk(int chunkX, int chunkY, int chunkZ)
+    {
+        Vector3Int chunkPos = new Vector3Int(chunkX * realChunkSize, chunkY * realChunkSize, chunkZ * realChunkSize);
+        Voxel[,,] chunkData = getChunkDataFromChunkPos(chunkPos);
+        VoxelMeshBuilder chunkI = Instantiate(chunkPrefab);
+        chunkI.transform.position = chunkPos;
+        chunkI.BuildChunk(chunkData);
+        builtChunks.Add(chunkPos, chunkI);
+    }
+
+    Vector3Int GetChunkPosOfVoxel(Vector3Int voxelPos)
+    {
+        int x = Mathf.FloorToInt((float)(voxelPos.x - 1) / realChunkSize);
+        int y = Mathf.FloorToInt((float)(voxelPos.y - 1) / realChunkSize);
+        int z = Mathf.FloorToInt((float)(voxelPos.z - 1) / realChunkSize);
+        return new Vector3Int(x, y, z);
+    }
+}
