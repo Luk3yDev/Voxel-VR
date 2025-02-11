31375c4a45e90235a260aabcd48e257ed81d71d7
Luk3yDev <lukemadsen501@gmail.com>
2025-02-11 15:00:54 +1300

    Works 👍

--- Assets/VoxelMeshGenerator/VoxelScripts/NoiseGenerator.cs
+++ Assets/VoxelMeshGenerator/VoxelScripts/NoiseGenerator.cs
@@ -12,8 +12,8 @@ public class NoiseGenerator : MonoBehaviour
     [SerializeField] float threashold;
     public Voxel GetVoxelAtPos(Vector3Int pos)
     {
-        float noiseVal = noise.snoise(new float3((float)pos.x, (float)pos.y, (float)pos.z) / noiseScale);
-        if (noiseVal < threashold)
+        float noiseVal = noise.snoise(new float3((float)pos.x, 0, (float)pos.z) / noiseScale);
+        if (((noiseVal + 1) * 20) > pos.y)
         {
             if (UnityEngine.Random.Range(1, 3) == 1) return fullVoxel;
             else return otherFullVoxel;
