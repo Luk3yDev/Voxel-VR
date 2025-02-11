15099bf7c6a0691df881e6a8ad7f319f1bfa27db
LukeyDev <lukemadsen501@gmail.com>
2025-02-11 17:14:51 +1300

    Terrain Destruction

---
+++ Assets/Scripts/ManipulateTerrain.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ManipulateTerrain : MonoBehaviour
+{
+    public InputActionProperty pinchAction;
+
+    [SerializeField] MapBuilder map;
+
+    [Header("Raycast Parameters")]
+    [SerializeField] LayerMask terrainLayer;
+    [SerializeField] float range;
+
+    [Header("Voxels")]
+    [SerializeField] Voxel air;
+
+    [Header("Visuals")]
+    [SerializeField] GameObject indicator;
+
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hitInfo, range, terrainLayer))
+        {
+            Vector3 targetPos = hitInfo.point - (hitInfo.normal * 0.5f);
+            Vector3Int voxelPos = new Vector3Int(Mathf.RoundToInt(targetPos.x), Mathf.RoundToInt(targetPos.y), Mathf.RoundToInt(targetPos.z));
+            indicator.SetActive(true);
+            indicator.transform.position = voxelPos;
+        }
+        else
+            indicator.SetActive(false);
+
+        float triggerValue = pinchAction.action.ReadValue<float>();
+        if (triggerValue > 0.5f)
+        {
+            DestroyVoxel();
+        }
+    }
+
+    void DestroyVoxel()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hitInfo, range, terrainLayer))
+        {
+            Vector3 targetPos = hitInfo.point - (hitInfo.normal * 0.5f);
+            Vector3Int voxelPos = new Vector3Int(Mathf.RoundToInt(targetPos.x), Mathf.RoundToInt(targetPos.y), Mathf.RoundToInt(targetPos.z));
+
+            map.SetVoxel(voxelPos, air);
+        }
+    }
+}
