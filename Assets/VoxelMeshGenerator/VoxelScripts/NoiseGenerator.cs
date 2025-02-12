ed3674222cf5f8abd6f75b55ce0d703d4f2f87f3
LukeyDev <lukemadsen501@gmail.com>
2025-02-12 17:38:25 +1300

    Island falloff

--- Assets/VoxelMeshGenerator/VoxelScripts/NoiseGenerator.cs
+++ Assets/VoxelMeshGenerator/VoxelScripts/NoiseGenerator.cs
@@ -20,31 +20,41 @@ public class NoiseGenerator : MonoBehaviour
     [SerializeField] float oddityOffset;
     int seed;
     int odditySeed;
+    MapBuilder mb;
 
     private void Awake()
     {
         UnityEngine.Random.InitState(System.DateTime.Now.Second);
         seed = UnityEngine.Random.Range(-1000, 1000);
         odditySeed = UnityEngine.Random.Range(-1000, 1000);
+        mb = GetComponent<MapBuilder>();
     }
 
     public Voxel GetVoxelAtPos(Vector3Int pos)
     {
+        int centerX = (mb.mapSize.x * mb.realChunkSize) / 2;
+        int centerZ = (mb.mapSize.z * mb.realChunkSize) / 2;
+
+        float distanceFromCenter = Mathf.Sqrt((pos.x - centerX) * (pos.x - centerX) + (pos.z - centerZ) * (pos.z - centerZ));
+        float falloff = Mathf.Clamp01(Mathf.Pow(distanceFromCenter / centerX, 2));
+
         float noiseVal = noise.snoise(new float3((float)pos.x + seed, 0, (float)pos.z + seed) / horizontalScale);
         float noiseValT = noise.snoise(new float3((float)pos.x + odditySeed, (float)pos.y + odditySeed, (float)pos.z + odditySeed) / oddityScale);
         noiseVal += (noiseValT + (oddityOffset - 0.5f)) * oddity;
-        if (((noiseVal + 1) * verticalScale) > pos.y)
+
+        float height = ((noiseVal + 1) * verticalScale * (1f - falloff));
+
+        if (height > pos.y + 3)
         {
             return stoneVoxel;
         }
-        if (((noiseVal + 1) * verticalScale) > pos.y-2
-            )
+        if (height > pos.y + 1)
         {
             return dirtVoxel;
         }
-        if (((noiseVal + 1) * verticalScale) > pos.y - 3)
+        if (height > pos.y)
         {           
-            if (UnityEngine.Random.Range(0, 100) == 0)
+            if (UnityEngine.Random.Range(0, 200) == 0)
             {
                 return rootVoxel;
             }
