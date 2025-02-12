aea8377c33641d9ccee917ce75770418ae4c7942
Luk3yDev <lukemadsen501@gmail.com>
2025-02-12 14:03:04 +1300

    3D Noise mixed in

--- Assets/VoxelMeshGenerator/VoxelScripts/NoiseGenerator.cs
+++ Assets/VoxelMeshGenerator/VoxelScripts/NoiseGenerator.cs
@@ -14,26 +14,34 @@ public class NoiseGenerator : MonoBehaviour
     [Header("Noise Params")]
     [SerializeField] float horizontalScale;
     [SerializeField] float verticalScale;
+    [SerializeField] float oddityScale;
+    [SerializeField] float oddity;
+    [SerializeField] float oddityOffset;
     int seed;
+    int odditySeed;
 
     private void Awake()
     {
         UnityEngine.Random.InitState(System.DateTime.Now.Second);
         seed = UnityEngine.Random.Range(-1000, 1000);
+        odditySeed = UnityEngine.Random.Range(-1000, 1000);
     }
 
     public Voxel GetVoxelAtPos(Vector3Int pos)
     {
         float noiseVal = noise.snoise(new float3((float)pos.x + seed, 0, (float)pos.z + seed) / horizontalScale);
+        float noiseValT = noise.snoise(new float3((float)pos.x + odditySeed, (float)pos.y + odditySeed, (float)pos.z + odditySeed) / oddityScale);
+        noiseVal += (noiseValT + (oddityOffset - 0.5f)) * oddity;
         if (((noiseVal + 1) * verticalScale) > pos.y)
         {
             return stoneVoxel;
         }
-        if (((noiseVal + 1) * verticalScale) > pos.y-1)
+        if (((noiseVal + 1) * verticalScale) > pos.y-2
+            )
         {
             return dirtVoxel;
         }
-        if (((noiseVal + 1) * verticalScale) > pos.y-2)
+        if (((noiseVal + 1) * verticalScale) > pos.y-3)
         {
             return grassVoxel;
         }
