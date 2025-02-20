dff1ee3049fe6ec60c624e1ae9ff68f7aa31c93c
LukeyDev <lukemadsen501@gmail.com>
2025-02-20 17:33:44 +1300

    FINALLY OH MY GODDDDD
    I fixed the stupid dictionary shtis

--- Assets/Scripts/Netcode/NetworkWorld.cs
+++ Assets/Scripts/Netcode/NetworkWorld.cs
@@ -7,19 +7,21 @@ public class NetworkWorld : NetworkBehaviour
 {
     MapBuilder world;
 
-    private void Start()
+    void Awake()
     {
         world = GetComponent<MapBuilder>();
     }
 
     [Rpc(SendTo.NotMe)]
-    void SetVoxelClientRpc(int x, int y, int z, int voxelIndex)
+    public void SetVoxelClientRpc(int x, int y, int z, int voxelIndex)
     {
         world.SetVoxel(new Vector3Int(x, y, z), VoxelIndexer.IndexToVoxel(voxelIndex));
     }
 
     public void NetworkSetVoxel(Vector3Int voxelPos, Voxel voxel)
     {
+        if (world == null) return;
+        if (!IsOwner) return;
         SetVoxelClientRpc(voxelPos.x, voxelPos.y, voxelPos.z, VoxelIndexer.VoxelToIndex(voxel));
     }
 }
